edbe418687044f3296099b023934863303d45c58
frankleafy <leafy@nospoon.nl>
2018-08-31 01:51:44 +0200

    fs

--- docs/js/state.js
+++ docs/js/state.js
@@ -1,10 +1,23 @@
  //init
  var singleChallengeUrl = "https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Current%20Challenges&filter=";
  var app;
+ var md = new Remarkable();
 
- window.onhashchange = function() { 
-    //code  
-    this.alert(window.location.pathname);
+ //Capture back/forward button clicks
+ window.onhashchange = function() 
+ { 
+    if(app.currentPage != "" && "#"+ app.currentPage != document.location.hash)
+    {
+       var page = document.location.hash.substring(1);
+        for (var i = 0; i < app.menuItems.length; i++) 
+        { 
+            if(app.menuItems[i].title == page)
+            {
+                setContentContainer(i);
+                break;
+            }
+        }
+    }
 }
 
  window.onload = function () {
@@ -12,28 +25,29 @@
         {
             el: '#app',
             data: {
-                name: 'Frank',
+                input: '# hello',
+                currentPage: '',
                 menuItems :[
-                    {title:"Home", pageTitle:"Home", hidden:false, section:"home", container:"home", data:[], content:"", loaded:false},
-                    {title:"Green", pageTitle:"Sustainability challenges", hidden:false, section:"green", container:"green", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Green%20Challenges", loaded:false},
-                    {title:"Social", pageTitle:"Social challenges", hidden:false, section:"social", container:"social", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Social%20Challenges", loaded:false},
-                    {title:"Commercial", pageTitle:"Commercial challenges", hidden:false, section:"commercial", container:"commercial", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Commercial%20Challenges", loaded:false},
-                    {title:"Favorites", pageTitle:"Your favorites", hidden:false, section:"favorites", container:"favorites", data:[], content:"", loaded:false},
-                    {title:"Details", pageTitle:"Details of this challenge", hidden:true, section:"detail", container:"detail", data:[], content:"", loaded:false},
-                    {title:"Apply", pageTitle:"Apply for a challenge", hidden:false, section:"apply", container:"apply", data:[], content:"", loaded:false},
-                    {title:"Events", pageTitle:"Oncoming events", hidden:false, section:"events", container:"events", data:[], content:"", loaded:false},
-                    {title:"About", pageTitle:"About MBO Challenges", hidden:false, section:"about", container:"about", data:[], content:"", loaded:false},
-                    {title:"Contact", pageTitle:"Contact us", hidden:false, section:"contact", container:"contact", data:[], content:"", loaded:false},
+                    {title:"Home", pageTitle:"Home", hidden:false, section:"home", container:"home", data:[], content:"", loaded:false, selected:true},
+                    {title:"Green", pageTitle:"Sustainability challenges", hidden:false, section:"green", container:"green", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Green%20Challenges", loaded:false, selected:false},
+                    {title:"Social", pageTitle:"Social challenges", hidden:false, section:"social", container:"social", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Social%20Challenges", loaded:false, selected:false},
+                    {title:"Commercial", pageTitle:"Commercial challenges", hidden:false, section:"commercial", container:"commercial", data:[], content:"https://script.google.com/macros/s/AKfycbx4cVgWqXOtoRqvX70nqhlP0N6dslt2uUoPTymyZwsB-cE81-H8/exec?view=Commercial%20Challenges", loaded:false, selected:false},
+                    {title:"Favorites", pageTitle:"Your favorites", hidden:false, section:"favorites", container:"favorites", data:[], content:"", loaded:false, selected:false},
+                    {title:"Details", pageTitle:"Details of this challenge", hidden:true, section:"detail", container:"detail", data:[], content:"", loaded:false, selected:false},
+                    {title:"Apply", pageTitle:"Apply for a challenge", hidden:false, section:"apply", container:"apply", data:[], content:"", loaded:false, selected:false},
+                    {title:"Events", pageTitle:"Oncoming events", hidden:false, section:"events", container:"events", data:[], content:"", loaded:false, selected:false},
+                    {title:"About", pageTitle:"About MBO Challenges", hidden:false, section:"about", container:"about", data:[], content:"", loaded:false, selected:false},
+                    {title:"Contact", pageTitle:"Contact us", hidden:false, section:"contact", container:"contact", data:[], content:"", loaded:false, selected:false},
                     ],
                 selectedItems: [],
                 itemDetails:[]
             },
             mounted: function(){
-                   // alert(localStorage.getItem('selected')[2]);
                    if(localStorage.bookmarks) this.selectedItems = JSON.parse(localStorage.bookmarks);
                 
             },
-            methods: {               
+            methods: 
+            {               
                 loadItems : function(target)
                 {
                     loadItems(target)
@@ -55,8 +69,19 @@
                     if(this.itemDetails.length > 0)this.itemDetails.pop();
                     this.itemDetails.push(challenge);      
                     setContentContainer(5);  
-                }
+                },
+                compiledMarkdown: function (source) 
+                {
+                  return marked(source, { sanitize: true })
+                },
+                update: _.debounce(function (e) {
+                    this.input = e.target.value
+                  }, 300)
                 
+            },
+            computed: 
+            {
+    
             }
         });
 
@@ -74,13 +99,18 @@ function setContentContainer(target)
         
         if(i == target)
         {
-            if(placeholder)placeholder.style.cssText = 'block';
+            app.currentPage = app.menuItems[i].title;
+            app.menuItems[i].selected = true;
+            //alert('update:'+app.menuItems[i].title);
+            if(placeholder)placeholder.style.cssText = 'block';           
         }
         else
         {
             if(placeholder)placeholder.style.display = 'none';
+            app.menuItems[i].selected = false;
         }
     }
+    
 }
 
   
